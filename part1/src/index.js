6994ed9dd56cdc7d1dc389f08f241381249bdbd8
endBunker <g.lanzillotta@protonmail.com>
2020-08-28 23:07:48 +0200

    Course information, part5

--- part1/src/index.js
+++ part1/src/index.js
@@ -28,22 +28,22 @@ const App = () => {
     );
   };
 
-  const Part = (props) => {
+  const Part = ({ name, exercises }) => {
     return (
       <div>
         <p>
-          {props.name} {props.exercises}
+          {name} {exercises}
         </p>
       </div>
     );
   };
 
-  const Content = (props) => {
+  const Content = ({ parts }) => {
     return (
       <div>
-        <Part name={props.props[0].name} exercises={props.props[0].exercises} />
-        <Part name={props.props[1].name} exercises={props.props[1].exercises} />
-        <Part name={props.props[2].name} exercises={props.props[2].exercises} />
+        <Part name={parts[0].name} exercises={parts[0].exercises} />
+        <Part name={parts[1].name} exercises={parts[1].exercises} />
+        <Part name={parts[2].name} exercises={parts[2].exercises} />
       </div>
     );
   };
@@ -64,7 +64,7 @@ const App = () => {
   return (
     <div>
       <Header course={course.name} />
-      <Content props={course.parts} />
+      <Content parts={course.parts} />
       <Total props={course.parts} />
     </div>
   );
