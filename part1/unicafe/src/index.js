749de1d0ff11a2dc1984c43b8d874cc2fe6b996c
endBunker <g.lanzillotta@protonmail.com>
2020-08-30 18:25:02 +0200

    Unicafe, step6

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,21 +2,41 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-const Statistics = (props) => {
-  if (props.total !== 0) {
-    return (
-      <div>
-        <h1>statistics</h1>
-        <p>good {props.good}</p>
-        <p>neutral {props.neutral}</p>
-        <p>bad {props.bad}</p>
-        <p>average {props.average}</p>
-        <p>positive {props.positive}</p>
-      </div>
-    );
-  } else {
-    return <p>No feedback given</p>;
-  }
+const Statistic = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
+};
+
+const Statistics = ({ good, bad, neutral, average, positive }) => {
+  return (
+    <div>
+      <table>
+        <tbody>
+          <Statistic text="good" value={good} />
+          <Statistic text="neutral" value={neutral} />
+          <Statistic text="bad" value={bad} />
+          <Statistic text="average" value={average} />
+          <Statistic text="positive" value={positive} />
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+const Button = (props) => {
+  return (
+    <div>
+      <button onClick={() => props.setGood(props.good + 1)}>good </button>
+      <button onClick={() => props.setNeutral(props.neutral + 1)}>
+        neutral
+      </button>
+      <button onClick={() => props.setBad(props.bad + 1)}>bad</button>
+    </div>
+  );
 };
 
 const App = () => {
@@ -27,23 +47,45 @@ const App = () => {
   let total = good + bad + neutral;
   const average = (good - bad) / total;
   const positive = (good * 100) / total;
+  if (total !== 0) {
+    return (
+      <div>
+        <h1>give feedback</h1>
+        <Button
+          good={good}
+          bad={bad}
+          neutral={neutral}
+          setGood={setGood}
+          setBad={setBad}
+          setNeutral={setNeutral}
+        />
 
-  return (
-    <div>
-      <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good </button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
-      <Statistics
-        total={total}
-        good={good}
-        bad={bad}
-        neutral={neutral}
-        average={average}
-        positive={positive}
-      />
-    </div>
-  );
+        <h1>Statistics</h1>
+        <Statistics
+          good={good}
+          bad={bad}
+          neutral={neutral}
+          average={average}
+          positive={positive}
+        />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>give feedback</h1>
+        <Button
+          good={good}
+          bad={bad}
+          neutral={neutral}
+          setGood={setGood}
+          setBad={setBad}
+          setNeutral={setNeutral}
+        />
+        <p>No feedback give</p>
+      </div>
+    );
+  }
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
