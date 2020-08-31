4d3dad1a1e7145652c4fc86d96ac7403b0ecf01b
endBunker <g.lanzillotta@protonmail.com>
2020-08-31 16:46:48 +0200

    Separate module

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -1,63 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-const Header = (props) => {
-  const { course } = props;
-
-  return (
-    <div>
-      <h2>{course}</h2>
-    </div>
-  );
-};
-
-const Part = (props) => {
-  const { name, exercises } = props;
-
-  return (
-    <div>
-      <p>
-        {name} {exercises}
-      </p>
-    </div>
-  );
-};
-
-const Content = (props) => {
-  const { parts } = props;
-
-  return (
-    <div>
-      {parts.map((part) => (
-        <Part name={part.name} exercises={part.exercises} />
-      ))}
-    </div>
-  );
-};
-
-const Total = (props) => {
-  const { parts } = props;
-  const total = parts
-    .map((part) => part.exercises)
-    .reduce((acc, cur) => acc + cur);
-
-  return (
-    <div>
-      <strong>Number of exercises {total}</strong>
-    </div>
-  );
-};
-
-const Course = (props) => {
-  const { course } = props;
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
-};
+import Course from "./Course";
 
 const App = () => {
   const courses = [
