10b8408448cef1510f1c6818cc3027a87fea6860
endBunker <g.lanzillotta@protonmail.com>
2020-09-02 22:50:21 +0200

    The Phonebook Step6

---
+++ part2/phonebook/src/components/Persons.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Persons = (props) => {
+  const { persons, search } = props;
+  const printPerson = (person) => {
+    return (
+      <div>
+        {person.name} {person.number}
+      </div>
+    );
+  };
+
+  return search === ""
+    ? persons.map(printPerson)
+    : persons
+        .filter((person) => person.name.toLowerCase() === search.toLowerCase())
+        .map(printPerson);
+};
+
+export default Persons;
