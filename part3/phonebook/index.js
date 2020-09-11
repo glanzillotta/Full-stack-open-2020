ec3738cb1b2e3fb8a2b72357e1f3c78799f515ad
endBunker <g.lanzillotta@protonmail.com>
2020-09-11 18:47:02 +0200

    Phonebook backend step4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -43,6 +43,12 @@ app.get("/api/persons/:id", (req, res) => {
   else res.status(404).end();
 });
 
+app.delete("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  persons.filter((person) => person.id != id);
+  res.status(204).end();
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
