e9714e1dc93747bad1e7699ed521e032e007cd19
Giuseppe Lanzillotta <g.lanzillotta@protonmail.com>
2020-11-07 17:39:48 +0100

    4.1 Blog list, step1

---
+++ part4/blog/index.js
@@ -0,0 +1,46 @@
+import http from 'http'
+import express, { json } from 'express'
+const app = express()
+import cors from 'cors'
+import mongoose from 'mongoose' 
+const { Schema, model, connect } = mongoose;
+import dotenv from 'dotenv'
+dotenv.config()
+
+const blogSchema = new Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = model('Blog', blogSchema)
+
+const mongoUrl = process.env.MONGODB_URI
+connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+app.use(cors())
+app.use(json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
