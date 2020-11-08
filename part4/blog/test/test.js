0b31be41243c2d1aee62cffb335658aab703a5fb
Giuseppe Lanzillotta <g.lanzillotta@protonmail.com>
2020-11-08 17:59:33 +0100

    4.4: helper functions and unit tests, step2

--- part4/blog/test/test.js
+++ part4/blog/test/test.js
@@ -81,8 +81,17 @@ describe("total likes", () => {
     },
   ];
 
-  test("when list has only one blog, equals the likes of that", () => {
+  test("total number of likes of all the articles", () => {
     const result = listHelper.totalLikes(blogs);
     expect(result).toBe(36);
   });
+
+  test("article with the bigger number of like", ()=>{
+    const result= listHelper.favoriteBlog(blogs);
+    expect(result).toEqual({
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      likes: 12
+    });
+  })
 });
--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,17 +1,31 @@
 const dummy = (blogs) => {
-    if (Array.isArray(blogs))
-        return 1; 
-  }
+  if (Array.isArray(blogs)) return 1;
+};
 
-const totalLikes = (blogs) =>{
-  let total=0;
-  blogs.forEach(blog =>{
+const totalLikes = (blogs) => {
+  let total = 0;
+  blogs.forEach((blog) => {
     total += blog.likes;
-  })
+  });
   return total;
-}
-  
+};
+
+const favoriteBlog = (blogs) => {
+  let idMax = 0,
+    max = 0;
+  blogs.forEach((blog) => {
+    if (blog.likes >= max) {
+      max = blog.likes;
+      idMax = blog._id;
+    }
+  });
+
+  const blog = blogs.find((blog) => blog._id === idMax);
+  return { title: blog.title, author: blog.author, likes: blog.likes };
+};
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog,
 };
