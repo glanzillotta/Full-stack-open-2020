fa195fcfc028fb8117508021550bdfb89256d1bc
Giuseppe Lanzillotta <g.lanzillotta@protonmail.com>
2020-11-08 17:13:09 +0100

    4.4: helper functions and unit tests, step2

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -2,7 +2,16 @@ const dummy = (blogs) => {
     if (Array.isArray(blogs))
         return 1; 
   }
+
+const totalLikes = (blogs) =>{
+  let total=0;
+  blogs.forEach(blog =>{
+    total += blog.likes;
+  })
+  return total;
+}
   
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 };
