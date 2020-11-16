6d88a94b2125674fe97fb8ef89c61b8f4027ee09
Giuseppe Lanzillotta <g.lanzillotta@protonmail.com>
2020-11-16 16:54:09 +0100

    5.7* Blog list frontend, step7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,31 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from "react";
 
-export default Blog
+const Blog = (props) => {
+  const { blog } = props;
+  const [visible, setVisible] = useState(false);
+  const hide = { display: visible ? "" : "none" };
+  const border = { border: "1px solid", margin: "2px" };
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  const handleLikes = () => {};
+
+  return (
+    <div style={border}>
+      {blog.title} {blog.author}{" "}
+      <input type="button" value="view" onClick={toggleVisibility} />
+      <div style={hide}>
+        {blog.url}
+        <div>
+          {blog.likes}{" "}
+          <input type="button" value="like" onClick={handleLikes} />
+        </div>
+        {blog.user !== undefined && blog.user.username}
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
