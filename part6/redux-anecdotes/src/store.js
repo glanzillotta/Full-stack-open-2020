bd04316c69524473d110171273a3cf22597ae612
Giuseppe Lanzillotta <g.lanzillotta@protonmail.com>
2020-11-23 17:29:47 +0100

    6.13 Anecdotes and the backend, step1

--- part6/redux-anecdotes/src/store.js
+++ part6/redux-anecdotes/src/store.js
@@ -1,5 +1,6 @@
 import { createStore, combineReducers  } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
+
 import anecdoteReducer from './reducers/anecdoteReducer'
 import notificationReducer from './reducers/notificationReducer'
 import filterReducer from './reducers/filterReducer'
