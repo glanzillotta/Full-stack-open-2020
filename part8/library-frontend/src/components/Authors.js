143d473757e665d1432914d927b3ae50d4bf7e82
Giuseppe Lanzillotta <g.lanzillotta@protonmail.com>
2020-12-08 13:53:12 +0100

    8.8: Authors view

---
+++ part8/library-frontend/src/components/Authors.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react'
+import { useQuery } from '@apollo/client'
+import { ALL_AUTHORS } from './queries'
+
+const Authors = (props) => {
+  const [authors, setAuthors] = useState([])
+  const {data} = useQuery(ALL_AUTHORS)
+
+  useEffect(() => {
+    if(data)
+      setAuthors(data.allAuthors)
+  }, [data])
+
+  if (!props.show) {
+    return null
+  }
+
+  if (!data) return null
+  
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>
+              born
+            </th>
+            <th>
+              books
+            </th>
+          </tr>
+          {authors.map(a =>
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+
+    </div>
+  )
+}
+
+export default Authors
