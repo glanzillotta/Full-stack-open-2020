e7afe443f9cb7a181807ee77fd6751d93e64f923
Giuseppe Lanzillotta <g.lanzillotta@protonmail.com>
2020-12-14 12:51:07 +0100

    9.3 Command line

--- part9/bmiCalculator.ts
+++ part9/bmiCalculator.ts
@@ -1,5 +1,24 @@
+interface InputBmi {
+    value1: number,
+    value2: number
+};
+
+const parseArgumentsBmi = (args: Array<string>): InputBmi => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+    if (args.length > 4) throw new Error('Too many arguments');
+
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+        return {
+            value1: Number(args[2]),
+            value2: Number(args[3])
+        }
+    } else {
+        throw new Error('Provided values were not numbers!');
+    }
+}
+
 const calculateBmi = (height: number, weight: number): string => {
-    const bmi = weight / ((height/100)**2);
+    const bmi = weight / ((height / 100) ** 2);
     switch (true) {
         case bmi > 0 && bmi < 15:
             return 'Very severely underweight ';
@@ -20,5 +39,9 @@ const calculateBmi = (height: number, weight: number): string => {
         default: return null;
     }
 }
-
-console.log(calculateBmi(180, 74))
+try {
+    const { value1, value2 } = parseArgumentsBmi(process.argv);
+    console.log(calculateBmi(value1, value2));
+} catch (e) {
+    console.log('Error, something bad happened, message: ', e.message)
+}
--- part9/exerciseCalculator.ts
+++ part9/exerciseCalculator.ts
@@ -8,33 +8,52 @@ interface Result {
     average: number
 };
 
+interface InputExercise {
+    targetAmountsDaily: number,
+    dailyExercisesHours: Array<number>
+};
+
+const parseArgumentsExercise = (args: Array<string>): InputExercise => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+
+    if (!isNaN(Number(args[2]))) {
+        return {
+            targetAmountsDaily: Number(args[2]),
+            dailyExercisesHours: args.filter((arg,i)=>i>2&&!isNaN(Number(arg))).map((arg) => Number(arg))
+        }
+    } else {
+        throw new Error('Provided values were not numbers!');
+    }
+}
+
+
 const calculateExercises = (dailyExercisesHours: Array<number>, targetAmountsDaily: number): Result => {
 
-    const periodLength=dailyExercisesHours.length;
-    const trainingDays=dailyExercisesHours.filter(hour=>hour!==0).length;
-    const average=dailyExercisesHours.reduce((a, b) => a + b)/dailyExercisesHours.length;
-    const success= average>=targetAmountsDaily?true:false;
-    const target=targetAmountsDaily;
-    const diff=targetAmountsDaily-average
+    const periodLength = dailyExercisesHours.length;
+    const trainingDays = dailyExercisesHours.filter(hour => hour !== 0).length;
+    const average = dailyExercisesHours.reduce((a, b) => a + b) / dailyExercisesHours.length;
+    const success = average >= targetAmountsDaily ? true : false;
+    const target = targetAmountsDaily;
+    const diff = targetAmountsDaily - average
     let rating: number;
     let ratingDescription: string;
-    switch(true){
-        case diff===0: 
-            rating=3;
-            ratingDescription='you did it';
+    switch (true) {
+        case diff === 0:
+            rating = 3;
+            ratingDescription = 'you did it';
             break;
-        case diff<0.5:
-            rating=2;
-            ratingDescription='not too bad but could be better';
+        case diff < 0.5:
+            rating = 2;
+            ratingDescription = 'not too bad but could be better';
             break;
-        case diff<1: 
-            rating=1
-            ratingDescription='come one we know you could do better';
+        case diff < 1:
+            rating = 1
+            ratingDescription = 'come one we know you could do better';
             break;
-        default: rating=0; break;
+        default: rating = 0; break;
     }
 
-    return{
+    return {
         periodLength,
         trainingDays,
         success,
@@ -45,6 +64,9 @@ const calculateExercises = (dailyExercisesHours: Array<number>, targetAmountsDai
     }
 }
 
-const dailyExercisesHours = [3, 0, 2, 4.5, 0, 3, 1]
-const targetAmountsDaily = 2
-console.log(calculateExercises(dailyExercisesHours, targetAmountsDaily))
+try {
+    const { targetAmountsDaily, dailyExercisesHours } = parseArgumentsExercise(process.argv);
+    console.log(calculateExercises(dailyExercisesHours, targetAmountsDaily))
+} catch (e) {
+    console.log('Error, something bad happened, message: ', e.message)
+}
